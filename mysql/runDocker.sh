5b84c2c56621bb34506ba867d190273364f4aa15
Shachar297 <thisishachar@gmail.com>
2022-02-15 14:44:29 +0200

    Initial commit

--- mysql/runDocker.sh
+++ mysql/runDocker.sh
@@ -2,4 +2,4 @@ docker run \
     -v /Users/shacharovadia/Desktop/Projects/chatbot/mysql \
     -d --name chatbot-sql \
     -p 3306:3306 -e \
-    MYSQL_ROOT_PASSWORD=5366987S mysql:latest
+    MYSQL_ROOT_PASSWORD=yourpass mysql:latest
